7b46df6214895695990cdb57e5c76ad447adbfa4
pcochoco <cochocolatecoo@gmail.com>
2025-03-14 00:26:57 +0900

    wip: jwt 회원가입 기능

---
+++ src/main/java/com/itda/moamoa/global/security/jwt/config/JwtSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.itda.moamoa.global.security.jwt.config;
+
+import com.itda.moamoa.global.security.jwt.filter.LoginFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class JwtSecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public JwtSecurityConfig(AuthenticationConfiguration authenticationConfiguration){
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+    //비밀번호 인코딩
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //csrf disable - session의 경우 필수 방어 필요, jwt는 stateless
+        http
+                .csrf((auth) -> auth.disable());
+        //form login disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth)->auth
+                        .requestMatchers("/login","/","/join").permitAll()
+
+                        //권한 가진 사람
+                        .requestMatchers("/admin").hasRole("ADMIN")
+
+                        //로그인 사용자만
+                        .anyRequest().authenticated());
+
+        //로그인 필터 추가
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
+        //세션 설정 -stateless로 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+
+
+    }
+}
---
+++ src/main/java/com/itda/moamoa/global/security/jwt/service/JoinService.java
@@ -0,0 +1,41 @@
+package com.itda.moamoa.global.security.jwt.service;
+
+import com.itda.moamoa.domain.user.entity.User;
+import com.itda.moamoa.domain.user.repository.UserRepository;
+import com.itda.moamoa.global.security.jwt.dto.JoinDTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public Boolean joinProcess(JoinDTO joinDTO){ //true false
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+        if(isExist){
+            return Boolean.FALSE; //username 중복
+        }
+
+        User user = User.builder()
+                .username(username)
+                //비밀번호 암호화 후 저장
+                .password(bCryptPasswordEncoder.encode(password))
+                .role("ROLE_ADMIN").build();
+
+        userRepository.save(user);
+        return Boolean.TRUE;
+    }
+
+
+
+
+}
