f4591dbb29d828bfea736dc3d52391b6bbbed155
skdudnayoung <xokny04188@naver.com>
2025-03-24 02:06:52 +0900

    wip: 합치는 과정

--- src/main/java/com/itda/moamoa/global/common/ErrorCode.java
+++ src/main/java/com/itda/moamoa/global/common/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, 400, "잘못된 요청입니다."),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 입력값입니다."),
     INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 타입입니다."),
+    ENTITY_NOT_FOUND(HttpStatus.BAD_REQUEST, 400, "요청한 리소스를 찾을 수 없습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, 400, "잘못된 요청입니다."),
 
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 401, "로그인이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, 403, "접근 권한이 없습니다."),
@@ -21,7 +23,11 @@ public enum ErrorCode {
 
     //마이페이지 구현하며 추가 - 사용자 관련 오류
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "사용자를 찾을 수 없습니다."),
-    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "파일 업로드에 실패했습니다.");
+    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "파일 업로드에 실패했습니다."),
+    
+    // 리뷰 관련 에러 코드
+    CANNOT_REVIEW_SELF(HttpStatus.BAD_REQUEST, 400, "자신의 리뷰를 작성할 수 없습니다."),
+    NOT_PARTICIPATED_TOGETHER(HttpStatus.BAD_REQUEST, 400, "함께 참여하지 않은 활동에 대한 리뷰를 작성할 수 없습니다.");
 
     private final HttpStatus status;
     private final int code;
