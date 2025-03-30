e744f19c7e32a9bf12603c16499fb4239111ea4d
Sjin618 <tkdsk740@gmail.com>
2025-03-30 17:44:57 +0900

    fix: UserController RestController로 변경

--- src/main/java/com/itda/moamoa/domain/user/controller/UserController.java
+++ src/main/java/com/itda/moamoa/domain/user/controller/UserController.java
@@ -7,14 +7,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
