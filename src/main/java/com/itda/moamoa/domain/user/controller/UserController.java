646c66d8ee18da902c580ee1d779fc24a973f8c6
Sjin618 <tkdsk740@gmail.com>
2025-03-30 16:05:12 +0900

    fix: 로그아웃 반환 타입 변경

--- src/main/java/com/itda/moamoa/domain/user/controller/UserController.java
+++ src/main/java/com/itda/moamoa/domain/user/controller/UserController.java
@@ -3,11 +3,16 @@ package com.itda.moamoa.domain.user.controller;
 import com.itda.moamoa.domain.user.entity.User;
 import com.itda.moamoa.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,19 +26,12 @@ public class UserController {
      * 탈퇴 후엔 로그아웃이 필요 -> 토큰들 다 만료시키기
      */
     @DeleteMapping("/auth/delete")
-    public String userDelete(@AuthenticationPrincipal(expression = "user") User user){
+    public ResponseEntity<?> userDelete(@AuthenticationPrincipal(expression = "user") User user){
         userService.deleteUser(user);
         // logout 으로 redirect 해서 LogoutFilter에 걸리게 한다.  -> access, refresh token 무효화
-        return "redirect:/api/auth/logout";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create("http://localhost:8080/api/auth/logout"));
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 
-    //비밀번호 찾기 메서드
-    //이메일, 새 비밀번호, 인증번호(OTP) 받아오기
-    //이메일로 인증번호 보내기 외부 API 사용
-    //만약 회원가입 안한 이메일이라면?
-    //social 로그인한 회원이라면?
-    @PatchMapping("/auth/password")
-    public String findPassword(){
-        return "ok";
-    }
 }
