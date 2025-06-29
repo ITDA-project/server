7dabdcd28fe1cfb391865116466c2df73cd42aaa
Sjin618 <tkdsk740@gmail.com>
2025-06-29 15:36:55 +0900

    fix: 메시지 전송 응답 수정

--- src/main/java/com/itda/moamoa/domain/chat/dto/MessageResponseDto.java
+++ src/main/java/com/itda/moamoa/domain/chat/dto/MessageResponseDto.java
@@ -1,5 +1,6 @@
 package com.itda.moamoa.domain.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
  */
 @Getter
 @Setter
+@AllArgsConstructor
 public class MessageResponseDto {
     private String sender;
     private String content;
