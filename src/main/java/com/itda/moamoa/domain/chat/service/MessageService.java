f2483e389a4e5702221a747c29c1d74be533b55a
Sjin618 <tkdsk740@gmail.com>
2025-06-30 18:01:03 +0900

    fix: lastMessage 갱신 버그 해결

--- src/main/java/com/itda/moamoa/domain/chat/service/MessageService.java
+++ src/main/java/com/itda/moamoa/domain/chat/service/MessageService.java
@@ -10,8 +10,10 @@ import com.itda.moamoa.domain.user.entity.User;
 import com.itda.moamoa.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MessageService {
     private final ChatMessageRepository chatMessageRepository;
