8efe4b57888958a16c398b6b97e9cdb13246a253
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-07-10 20:14:51 +0900

    fix: add chat notification dto, repository

---
+++ src/main/java/com/itda/moamoa/domain/chatnotification/dto/ChatNotificationDto.java
@@ -0,0 +1,4 @@
+package com.itda.moamoa.domain.chatnotification.dto;
+
+public class ChatNotificationDto {
+}
---
+++ src/main/java/com/itda/moamoa/domain/chatnotification/repository/ChatNotificationRepository.java
@@ -0,0 +1,4 @@
+package com.itda.moamoa.domain.chatnotification.repository;
+
+public class ChatNotificationRepository {
+}
